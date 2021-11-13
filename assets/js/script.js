1b5efe149a8ba9b11a5527ffbad5541c562f5cf0
vlsulliv <vlsulliv@yahoo.com>
2021-11-13 08:34:12 -0600

    finish html setup and begin js

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const day = moment().format("MMMM Do YYYY");
+const timeBlock = document.getElementById("class");
+timeBlock.innerHTML(".day")
+
+const now = moment().hour();
+
+
+// document.getElementById(" ").innerHTML = localStorage.getItem(" ")
+
+const saveBtn = document.getElementById("saveBtn");
+saveBtn.addEventListener("onClick", {
+    document.getElementById("")
+});
+localStorage.setItem(hour);
+
+// myStorage = window.localStorage;

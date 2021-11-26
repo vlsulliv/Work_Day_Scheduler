491f4b14c364aadf9a5e18e56ebe15eb262c853e
vince <vlsullivanhou@gmail.com>
2021-11-25 20:45:42 -0600

    Refactor HTML

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# calendar
+# work day scheduler 
 
 ## Summary
 GIVEN I am using a daily planner to create a schedule
@@ -15,3 +15,22 @@ THEN the text for that event is saved in local storage
 WHEN I refresh the page
 THEN the saved events persist
 ##
+
+
+
+## 
+
+
+## Contact Me
+[![Email: Yahoo](vlsulliv@yahoo.com)](www.yahoo.com)<br>
+[![DM: Slack](vince_)](www.slack.com)<br>
+[![Social Media: linkedIn](https://linkedin.com/vlsulliv/)](www.linkedin.com)<br>
+[![Projects: GitHub](https://github.com/vlsulliv)](www.github.com)<br>
+
+
+
+## License
+
+### <span><b>MIT</b></span><br>
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)<br>
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,87 @@
-const day = moment().format("MMMM Do YYYY");
-const timeBlock = document.getElementById("class");
-timeBlock.innerHTML(".day")
+/*=============================================
+=            SCRIPT TO CHANGE HTML           =
+=============================================*/
+moment().format();
+let day = moment().format('MMM DD, YYYY');
+console.log(moment().format('LT'));
 
-const now = moment().hour();
+let date = document.getElementById('currentDay');
+date.innerHTML = day
+
+document.getElementById('9am').style.color = "lightblue";
 
 
-// document.getElementById(" ").innerHTML = localStorage.getItem(" ")
 
-const saveBtn = document.getElementById("saveBtn");
-saveBtn.addEventListener("onClick", {
-    document.getElementById("")
-});
-localStorage.setItem(hour);
+// ! DISPLAY CURRENT DATE 
+let m = moment();
+// let day = moment().format('MMMM Do YYYY'); 
 
-// myStorage = window.localStorage;
+const timeBlock = document.getElementById("currentDay");
+timeBlock.innerHTML = day;
+
+let time = m.format('LT'); // H:M AM|PM
+console.log(time);
+
+
+// ! USE DOM APIs TO REFERENCE ELEMENTS
+// ! DECLARE VARIABLES/ INITIALIZE WITH ELEMENTS
+let table = document.getElementById('table');
+// let formData = document.querySelector('textarea');
+let store = hour => { localStorage.setItem(hour) };
+
+// ! FUNCTINON TO ADD QUERY ADD QUERY SELECTOR TO ALL BUTTONS
+// ! stop default behavior and call another function
+document.querySelectorAll('.saveBtn').forEach(item =>{
+    item.addEventListener('click', event => {
+    event.preventDefault()
+        let formData = document.getAttributeById.previousSibling.value
+        let myKey = document.getAttributeById("9AM")
+        localStorage.setItem(myKey, formData );
+    })
+})
+let retrieve = getHour => { localStorage.getItem(form)}
+
+
+
+
+
+
+
+
+        // ! ADD EVENT LISTENER TO ALL BUTTONS 
+/*
+  *  -----------------------------------------------------------------
+  *  const clickHandle
+  *      let elmnt = event.target.getElementbyClass('saveBtn');
+  *          addListeners.setAttribute("mybtn");
+  *          if (event.target.mathces('button')) {
+  *          count++;
+  *          event.target.setAttribute('saveBtn', count);
+  *          event.target.textContent = `Clicks: ${count}`;
+  *      }
+  *  let saveBtn = event.target.getAttribute('mybtn');
+  *  
+  *  -----------------------------------------------------------------
+  *  saveBtn.querySelectorAll(`button[class^=saveBtn]`);
+  *  btns.forEach(btn => {
+  *      btn.addEventListener('click', () => {
+  *          console.log(target.id);
+  *      }
+  *  )
+  *  -----------------------------------------------------------------
+  *  
+  *  let pt = function print(x) {
+  *      if (x || typeof(String)){
+  *          console.log(x)
+  *      } 
+  *  }
+  *  -----------------------------------------------------------------
+  * for( i = 0; i < time.lengeth; i++) {
+  *  time[i].addEventListener('click', clickHandler)
+  * 
+  *  let blockHour = document.getElementsByClassName('block-hour');
+  *  let ptag = document.getElementsByClassName('col-1');
+  *  let form = document.getElement('textarea');
+  * 
+  */
+ 

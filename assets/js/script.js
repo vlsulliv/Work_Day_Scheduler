e759befb32291816589954d9c1d10b7b5f445cba
vlsulliv@yahoo.com <vlsulliv@yahoo.com>
2021-12-09 23:46:08 -0600

    turn in

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 moment().format();
-console.log(moment().format('LT'));
+console.log(moment().format('LTS'));
 const date = moment().format('MMM DD, YYYY');
 const now = new Date();
-let hour = moment().format('H')
+// let hour = moment().format('H')
 document.getElementById('currentDay').innerHTML = date;
 
-/*======== STORAGE ============*/
 
+/*======== STORAGE ============*/
 console.log('1')
 document.getElementById("btn").addEventListener("click", function() {
     let text = document.getElementsByClassName('text');
@@ -14,16 +14,39 @@ document.getElementById("btn").addEventListener("click", function() {
     localStorage.setItem(hour, text)
 })
 
-
 function retrieve(){
     for (var i=0; i<Array.length; i+1){
         console.log(array[0])
     }
 }
 
+document.getElementById('')
+
 /*======== SCRIPT TO CHANGE HTML ============*/
-var div_list = document.querySelectorAll('textarea')
-var div_array=[...div_list];
+let logic = document.getElementsByClassName('block')
+
+var btn = document.g
+const ta = document.getElementById('task')
+var divs = document.querySelectorAll('div'), i;
+var button = document.getElementById('logic')
+let hour = "9";
+
+for (i = 0; i < divs.length; ++i) {
+  divs[i].style.color = "green";
+  console.log('btn')
+//   button.classList.add('past')
+//   if (btn == hour) {
+//       ta.classList.add('present');
+//   } else if (btn > hour) {
+//     button.classList.remove('present');
+//     button.classList.add('future');
+//   } else {
+//     button.classList.add('past');
+//   }
+}
+
+// var div_list = document.querySelectorAll('textarea')
+// var div_array=[...div_list];
 
 let present = function() {
         console.log(change);
@@ -39,3 +62,4 @@ let future = function() {
     console.log(change);
     change.classList.add("future");
 }
+
